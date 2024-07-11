a1e4735fefcea6ec67f52b1bfa945ffc628824d8
Gxizai <2046620162@qq.com>
2024-07-11 16:15:18 +0800

    saas模块相关代码开发

--- auth/src/main/java/com/ec/auth/security/filter/JwtAuthenticationTokenFilter.java
+++ auth/src/main/java/com/ec/auth/security/filter/JwtAuthenticationTokenFilter.java
@@ -23,6 +23,22 @@ import java.io.IOException;
  *
  * @author ec
  */
+
+/**
+ * <h3>JWT结构</h3>
+ * <p>就是令牌token，是一个String字符串，由3部分组成，中间用点隔开</p>
+ * <p>
+ *     <h3>令牌组成</h3>
+ *     <p>
+ *         1. 标头(Header)有令牌的类型和所使用的签名算法，如HMAC、SHA256、RSA；
+ *         使用Base64编码组成；（Base64是一种编码，不是一种加密过程，可以被翻译成原来的样子）<br>
+ *         2. 有效载荷(Payload)有效负载，包含声明；声明是有关实体（通常是用户）和其他数据的声明，
+ *         不放用户敏感的信息，如密码。同样使用Base64编码<br>
+ *         3. 签名(Signature)前面两部分都使用Base64进行编码，前端可以解开知道里面的信息。Signature需要使用编码后的header和payload
+ * 加上我们提供的一个密钥，使用header中指定的签名算法(HS256)进行签名。签名的作用是保证JWT没有被篡改过<br>
+ *     </p>
+ * </p>
+ */
 @Component
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Autowired
